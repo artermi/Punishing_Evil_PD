ebac839bb2bad803c3ced3e42785d558f9fb4ec6
artermi <ericcheng230@gmail.com>
2024-04-27 13:58:32 +0100

    gjogr; ;

--- Evil_PD.h
+++ Evil_PD.h
@@ -11,10 +11,10 @@
 
 
 #define K 1.2
-#define L 600
+#define L 1400
 #define LL (L*L)
-#define ITER 12001
-#define GAP 10
+#define ITER 8001
+#define GAP 100
 #define PERT 0.02
 
 class Evil_PD{
@@ -39,8 +39,11 @@ public:
 
 	Evil_PD(const double t, const double w, const double G, const double u,const double s,
 	const bool prep = false,const bool Grid = false, const int pp = RAND_MAX);
+
 	~Evil_PD();
 	//double unit_game(const int cent,const int target);
 	double centre_game(const int cent);
-	int game(bool ptf);
+	int game(bool ptf, bool avg = false);
+	int game2(bool ptf);
+	int calc_avg();
 };
